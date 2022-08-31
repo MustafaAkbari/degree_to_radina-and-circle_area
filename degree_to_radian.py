1e8b60e440aad6829ec946589ac90c0ddacba597
Humayan Al Rosid <63096143+humayanalrosid@users.noreply.github.com>
2022-08-31 17:14:53 +0530

    update converter program

--- degree_to_radian.py
+++ degree_to_radian.py
@@ -2,23 +2,24 @@ from math import pi
 
 # to achieve radian of a degree we should multiply degree to PI/180
 # to achieve degree of a radian we should multiply radian to 180/PI
-my_pi = pi
 
-while True:
-    choice = input("For degree to radian pres 'd',"
-                   " for radian to degree press 'r': \n".lower())
+def ConvertToRadianDegree(choice):
+
     if choice == "d":
-        degree_to_radian = int(input("Enter a degree: "))
-        radian = degree_to_radian * my_pi / 180
-        print(f"Radian of {degree_to_radian} degree is {radian}")
-        break
+        degree_to_radian = float(input("Enter a degree: "))
+        radian = degree_to_radian * (pi / 180)
+        return f"{degree_to_radian} Deg is {round(radian, 6)} Rad"
     elif choice == "r":
         radian_to_degree = float(input("Enter a radian: "))
-        degree = radian_to_degree * 180/my_pi
-        print(f"Degree of {radian_to_degree} radian is {int(degree)}")
-        break
+        degree = radian_to_degree * (180 / pi)
+        return f"{radian_to_degree} Rad is {round(degree, 4)} Deg"
     else:
-        print("Wrong choice!!!")
+        return "Wrong choice!!!"
+
+userChoice = input("Press 'd' to convert degrees to radians, and 'r' to convert radians to degrees : ").lower()
+
+result = ConvertToRadianDegree(userChoice)
+print(result)
 
 
 
